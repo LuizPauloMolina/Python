544a66a7c04528467154c989e0dbe51a15638292
LuizPauloMolina <79719743+LuizPauloMolina@users.noreply.github.com>
2021-02-26 22:11:29 -0300

    Add files via upload

---
+++ Data Types.py
@@ -0,0 +1,50 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # DATA TYPE - PYTHON
+
+# In[1]:
+
+
+a=5
+b=5.5
+c="why"
+d=True
+A=10.5
+
+
+# In[2]:
+
+
+type(a)
+
+
+# In[3]:
+
+
+type(b)
+
+
+# In[4]:
+
+
+type(c)
+
+
+# In[5]:
+
+
+type(d)
+
+
+# In[6]:
+
+
+type(A) #a!=A
+
+
+# In[ ]:
+
+
+
+
